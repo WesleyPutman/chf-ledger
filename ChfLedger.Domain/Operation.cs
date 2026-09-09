68f7934cbc52cbf4b836e9ab031192458a97eb7c
Wesley Putman <barryvert@gmail.com>
2026-09-09 14:17:44 +0200

    refactor: rend Operation materialisable par EF Core

--- ChfLedger.Domain/Operation.cs
+++ ChfLedger.Domain/Operation.cs
@@ -2,7 +2,11 @@ namespace ChfLedger.Domain;
 
 public class Operation
 {
-    public IReadOnlyList<Mouvement> Mouvements { get; }
+	public IReadOnlyList<Mouvement> Mouvements => _mouvements;
+	private readonly List<Mouvement> _mouvements = new();
+	// Constructeur privé dédié à l'usage d'EF afin de ne pas dépendre des Mouvements à la création de l'objet.
+	// Aucun appelant dans la codebase, ne pas supprimer
+	private Operation() { }
     public Operation(IEnumerable<Mouvement> mouvements)
     {
         var liste = mouvements.ToList();
@@ -12,6 +16,7 @@ public class Operation
         if (liste.Sum(m => m.Montant) != 0)
             throw new ArgumentException(
                 "La somme des mouvements doit faire 0", nameof(mouvements));
-        Mouvements = liste;
+        _mouvements.AddRange(liste);
     }
 }
+
